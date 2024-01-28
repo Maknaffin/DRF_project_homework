78cb3a135fdd27961ca7b0cb2cfae69bfeb2152c
timofey khlystov <tima.khlystov99@mail.ru>
2024-01-28 18:03:00 +0300

    Выполнил задание 25.2

--- drfsite/materials/serializers.py
+++ drfsite/materials/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 from rest_framework.relations import SlugRelatedField
 
-from materials.models import Course, Lesson, Payments
+from materials.models import Course, Lesson, Payments, Subscription
+from materials.validators import URLValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -11,14 +12,19 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+        validators = [URLValidator(field='link_to_video')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
     count_lessons = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True)
+    subscription = serializers.SerializerMethodField()
 
     def get_count_lessons(self, instance):
         return instance.lessons.all().count()
+
+    def get_subscription(self, instance):
+        return Subscription.objects.filter(course=instance, user=self.context['request'].user).exists()
 
     class Meta:
         model = Course
@@ -28,4 +34,11 @@ class CourseSerializer(serializers.ModelSerializer):
 class PaymentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payments
+        fields = '__all__'
+
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
         fields = '__all__'
--- drfsite/materials/tests.py
+++ drfsite/materials/tests.py
@@ -1,3 +1,151 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from materials.models import Lesson, Course, Subscription
+from users.models import User
+
+
+class LessonTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(email='test@sky.pro', is_active=True)
+        self.user.set_password('test_password')
+        self.user.save()
+        self.client.force_authenticate(user=self.user)
+        self.course = Course.objects.create(title='Test course')
+        self.lesson = Lesson.objects.create(
+            title='Test lesson',
+            course=self.course,
+            owner=self.user,
+        )
+
+    def test_create_lesson(self):
+        """Тестирование создания урока"""
+        data = {
+            'title': self.lesson,
+            'course': self.course,
+            'link_to_video': 'https://www.youtube.com/watch?v=i-uvtDKeFgE&list=PLA0M1Bcd0w8xZA3Kl1fYmOH_MfLpiYMRs'
+        }
+        response = self.client.post(
+            reverse('materials:lesson-create'),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_list_lesson(self):
+        """Тестирование списка уроков"""
+        response = self.client.get(
+            reverse('materials:lesson-list')
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_retrieve_lesson(self):
+        """Тестирование вывода одного урока"""
+        response = self.client.get(
+            reverse('materials:lesson-get', args=[self.lesson.id])
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_update_lesson(self):
+        """Тестирование редактирования уроков"""
+        data = {
+            'title': 'Test update title',
+            'description': 'Test update description',
+            'course': self.course,
+            'link_to_video': 'https://www.youtube.com/watch?v=i-uvtDKeFgE&list=PLA0M1Bcd0w8xZA3Kl1fYmOH_MfLpiYMRs'
+        }
+        response = self.client.put(
+            reverse('materials:lesson-update', args=[self.lesson.id]),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_destroy_lesson(self):
+        """Тестирование удаления уроков"""
+        response = self.client.delete(
+            reverse('materials:lesson-delete', args=[self.lesson.id])
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
+
+    def tearDown(self):
+        pass
+
+
+class SubscriptionTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.user = User.objects.create(email='test@sky.pro', is_active=True)
+        self.user.set_password('test_password')
+        self.user.save()
+        self.client.force_authenticate(user=self.user)
+        self.course = Course.objects.create(title='Test course')
+        self.lesson = Lesson.objects.create(
+            title='Test lesson',
+            course=self.course,
+            owner=self.user,
+        )
+        self.subscription = Subscription.objects.create(
+            user=self.user,
+            course=self.course,
+        )
+
+    def test_create_subscription(self):
+        """Тестирование создания подписки"""
+
+        data = {
+            'user': self.user.id,
+            'course': self.course.id,
+        }
+        response = self.client.post(
+            reverse('materials:subscription-create'),
+            data=data
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+    def test_list_subscription(self):
+        """Тестирование списка подписок"""
+        response = self.client.get(
+            reverse('materials:subscription-list')
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_destroy_subscription(self):
+        """Тестирование удаления подписок"""
+        response = self.client.delete(
+            reverse('materials:subscription-delete', args=[self.subscription.id])
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
+    def tearDown(self):
+        pass
